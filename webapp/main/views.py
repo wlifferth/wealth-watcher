fb7b5b32c194f5af3dda19a21afbbdde8b338e69
Will Lifferth <wlifferth@icloud.com>
2018-02-17 15:37:08 -0500

    Added a splash page; closes #8

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello world!")
+def splash(request):
+    context = {}
+    return render(request, 'main/splash.html', context)
