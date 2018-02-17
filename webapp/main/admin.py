039a39c8060ef2a116922ffdc0c22e76b7963191
Will Lifferth <wlifferth@icloud.com>
2018-02-17 12:15:45 -0500

    Created models and added to .gitignore; closes #17

--- webapp/main/admin.py
+++ webapp/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfile, Account, Purchase, Merchant, Transfer
+
+admin.site.register(UserProfile)
+admin.site.register(Account)
+admin.site.register(Purchase)
+admin.site.register(Merchant)
+admin.site.register(Transfer)
