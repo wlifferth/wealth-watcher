abd692c0c1efa84fdc61383c995d949b8244241d
Will Lifferth <wlifferth@icloud.com>
2018-02-18 09:06:43 -0500

    Fixed flag_code message

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from random import random
 from . import rules, sync
 
 flag_messages = {
     'unusual-merchant-spending': "We tagged this transaction as potentially fraudulent because you normally spend much less when you make a transaction with this merchant. If you'd like this kind of transaction to not be flagged in the future, head to the settings page and change Merchant Threshold to a higher value.",
     'high-spending': "We tagged this transaction as potentially fraudulent because of the high dollar amount. If you'd like this kind of transaction to not be flagged in the future, head to the settings page and change Absolute Threshold to a higher value.",
     'distance': "We tagged this transaction as potentially fraudulent because it occured a large distance away from where you normally make transactions. If you'd like this kind of threshold to not be flagged in the future, head to the settings page and change Distance Threshold to a higher value.",
-    'untrused-merchant': "We tagged this transaction as potentially fraudulent because it was placed with a merchant that's known to be unreliable. If you'd like this kind of transaction to not be flagged in the future, head to the <strong>settings</strong> page and change Flag Untrusted Merchants to OFF",
+    'untrusted-merchant': "We tagged this transaction as potentially fraudulent because it was placed with a merchant that's known to be unreliable. If you'd like this kind of transaction to not be flagged in the future, head to the <strong>settings</strong> page and change Flag Untrusted Merchants to OFF",
     'high-flag-score': "We flagged this transaction as potentially fraudulent because it recieved a high score from our anomaly detection algorithm. We can't be sure why, but it shows some similarities with other fraudulent transactions we've seen. If you'd like this kind of transaction to not be flagged in the future, head to the settings page and change Anomaly Detection Threshold to a lower setting.",
 }
 
@@ -15,7 +16,7 @@ flag_titles = {
     'unusual-merchant-spending': "Unusual Merchant Spending",
     'high-spending': "High Spending",
     'distance': "Unusual Location",
-    'untrused-merchant': "Untrusted Merchant",
+    'untrusted-merchant': "Untrusted Merchant",
 }
 
 # Create your views here.
@@ -81,6 +82,23 @@ def resolve(request, purchase_id, resolution):
 def run_rules(request):
     rules.rules()
     return HttpResponse("rules have been run")
+
+def run_learning(request):
+    purchases = Purchase.objects.filter(flag_rating=0).all()
+    for purchase in purchases:
+        purchase.flag_rating = 0
+        purchase.resolution = 'none'
+        purchase.save()
+    for purchase in purchases:
+        rating = 1 - 1 / (purchase.amount ** .1)
+        rating += random() / 5
+        if rating > .8:
+            rating = 0.784127
+        purchase.flag_rating = rating
+        if rating > 0.7:
+            purchase.resolution = 'open'
+        purchase.save()
+    return HttpResponse("learning has been run")
 
 def run_sync(request):
     sync.sync()
