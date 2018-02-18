0d4ab4a3447931c7761f5cd68b4364f63b35be80
Brent Hurst <seqvalley77@aol.com>
2018-02-17 20:03:59 -0500

    fixed putpurchasesincloud

--- data-gen/putpurchasesincloud.py
+++ data-gen/putpurchasesincloud.py
@@ -4,5 +4,5 @@ import string
 with open('transactions','r') as f:
     for line in f:
         l = line.split(',')
-        Purchase.Create("5a88c0af6514d52c7774b549",l[1],'',l[0],float(l[2]),'','')
+        rv = Purchase.Create("5a88c0af6514d52c7774b549",l[1],'balance',l[0],float(l[2]),'completed',' ')
 
