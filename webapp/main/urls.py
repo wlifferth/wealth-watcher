5b4df6c2ef75354345c6a657d1723cce0bfabc75
Will Lifferth <wlifferth@icloud.com>
2018-02-17 20:07:08 -0500

    Updated models and added data views

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.splash, name='splash'),
+    path('users/', views.users, name='users'),
+    path('user/<str:user_id>/', views.user, name='user'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
